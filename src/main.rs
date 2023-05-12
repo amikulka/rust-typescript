18653879f4e7830b6b39f1c8dbbcbd0fb49e21a4
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-05-12 13:23:09 -0600

    shows off Options

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
-struct Custom {
-    age: usize,
-    name: String,
+fn multiply(num: Option<usize>) -> Option<usize> {
+    return Some(num? * 5);
 }
 
-enum Item {
-    Number(usize),
-    String(String),
-    MyCustom(Custom),
-}
-
-fn append(items: &mut Vec<Item>) {
-    items.push(Item::String("hello, FEM".into()))
-}
-fn main() {
-    let mut items: Vec<Item> = vec![];
-    append(&mut items);
-
-    //doesnt work
-    // let mut other: Vec<usize> = vec![];
-    // append(&mut other);
-}
+fn main() {}
