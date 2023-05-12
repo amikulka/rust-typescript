bd2205d519a57225025293398c724f225ca2eb0a
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-05-12 12:38:19 -0600

    example of ts failing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,58 @@
-import fs from 'fs'
+type Custom = {
+  age: number
+  name: string
+}
+type Item = number | string | Custom
 
-let list = [1, 2, 3]
+function append(items: Item[]) {
+  items.push('hello fem!')
+}
+const items: Item[] = []
+console.log(items)
+append(items)
+console.log(items)
 
-console.log(list.map((num) => num + 1))
+const numbers: number[] = []
+console.log(numbers)
+append(numbers)
+console.log(numbers)
 
-fs.readFileSync('lines')
-  .toString()
-  .split('\n')
-  .filter((_, i) => i % 2 === 0)
-  .filter((_, i) => i > 1 && i < 4)
-  .forEach((line) => {
-    console.log(line)
-  })
+/* basic example of enums */
+
+// enum Color {
+//   Red,
+//   Green,
+//   Blue,
+//   Yellow,
+// }
+
+// function printColor(color: Color) {
+//   switch (color) {
+//     case Color.Red:
+//       console.log('red')
+//       break
+//     case Color.Blue:
+//       console.log('ble')
+//       break
+//     case Color.Green:
+//       console.log('green')
+//       break
+//   }
+// }
+
+// printColor(Color.Yellow)
+
+// import fs from 'fs'
+
+// let list = [1, 2, 3]
+
+// console.log(list.map((num) => num + 1))
+
+// fs.readFileSync('lines')
+//   .toString()
+//   .split('\n')
+//   .filter((_, i) => i % 2 === 0)
+//   .filter((_, i) => i > 1 && i < 4)
+//   .forEach((line) => {
+//     console.log(line)
+//   })
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
-fn main() {
-    let file = std::fs::read_to_string("lines").unwrap();
-
-    file.lines()
-        .enumerate()
-        .filter(|(idx, _)| idx % 2 == 0)
-        .skip(2)
-        .take(2)
-        .for_each(|(_, line)| println!("{}", line))
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow,
 }
+impl Color {
+    fn is_green(&self) -> bool {
+        if let Color::Green = self {
+            return true;
+        }
+        return false;
+    }
+    fn is_green_parts(&self) -> bool {
+        match self {
+            Color::Red => return false,
+            Color::Green => return false,
+            Color::Blue => return true,
+            Color::Yellow => return true,
+        }
+    }
+}
+fn print_color(color: Color) {
+    match color {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Blue => println!("blue"),
+        Color::Yellow => println!("yellow"),
+    }
+}
+fn main() {
+    let foo = Color::Green;
+
+    foo.is_green()
+}
+
+// fn main() {
+//     let file = std::fs::read_to_string("lines").unwrap();
+
+//     file.lines()
+//         .enumerate()
+//         .filter(|(idx, _)| idx % 2 == 0)
+//         .skip(2)
+//         .take(2)
+//         .for_each(|(_, line)| println!("{}", line))
+// }
