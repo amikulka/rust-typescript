c7766702a5a002892dc62ab9f9749b726cea5ab3
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-05-12 11:52:46 -0600

    example of cool map

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import fs from 'fs'
+
 let list = [1, 2, 3]
 
 console.log(list.map((num) => num + 1))
+
+fs.readFileSync('lines')
+  .toString()
+  .split('\n')
+  .filter((_, i) => i % 2 === 0)
+  .filter((_, i) => i > 1 && i < 4)
+  .forEach((line) => {
+    console.log(line)
+  })
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
-    let data = vec![1, 2, 3];
-    let mut foo = data.iter().map(|x| x + 1);
+    let file = std::fs::read_to_string("lines").unwrap();
 
-    let mut new_vector = vec![];
-
-    while let Some(x) = foo.next() {
-        new_vector.push(x);
-    }
-
-    println!("{:?}", new_vector)
+    file.lines()
+        .enumerate()
+        .filter(|(idx, _)| idx % 2 == 0)
+        .skip(2)
+        .take(2)
+        .for_each(|(_, line)| println!("{}", line))
 }
