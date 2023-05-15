7cb1f529bcda2391f0c72bb780b3c9ee25436fca
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-05-15 10:41:10 -0600

    more complex example

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,11 @@ const fileName = process.argv[2]
 fs.readFileSync(fileName)
   .toString()
   .split('\n')
-  .forEach((line) => console.log(line))
+  .forEach((line) => {
+    let print = parseInt(line)
+    if (isNaN(print)) {
+      console.log('Line not a number')
+    } else {
+      console.log(print)
+    }
+  })
